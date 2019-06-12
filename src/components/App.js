d5e34474cd83cd2aa827b6b6e3ef4dd3824574f4
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-11 20:50:40 -0400

    added footer component and fixed positions

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Home from './home'
 import About from './About'
 import Projects from './Projects'
 import Contact from './Contact'
+import Footer from './Footer'
 
 class App extends Component{
 
@@ -18,24 +19,7 @@ class App extends Component{
         <About />
         <Projects />
         <Contact />
-        <footer>&#160; <p className='footer-text'>Jason Pallone</p>
-        <a href="mailto:jasonpallone7593@gmail.com" rel='noopener noreferrer'  alt='gmail logo'
-      class="fa fa-google-plus-square"><br></br>
-    </a>
-
-    <a href="https://github.com/Jason-Pallone/" rel='noopener noreferrer'  alt="github logo"
-     target='_blank' class="fa github">&#xf092;
-    </a>
-
-    <a href="https://www.linkedin.com/in/jason-pallone-14070a187/" rel='noopener noreferrer' alt='linkedin logo' 
-    target='_blank' class="fa fa-linkedin-square" >
-    </a>
-
-    <a href="https://twitter.com/JasonPallone?s=09" rel='noopener noreferrer' alt="twitter icon"
-     target='_blank' class="fa fa-twitter-square">
-    </a>
-        
-        </footer>
+        <Footer />
       </div>
    )}
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer =()=>
+
+
+    <footer>
+
+        <p className='footer-text'> &#160; Jason Pallone</p>
+
+            <a href="mailto:jasonpallone7593@gmail.com" rel='noopener noreferrer'  alt='gmail logo'
+            class="fa fa-google-plus-square"><br></br>
+            </a>
+
+            <a href="https://github.com/Jason-Pallone/" rel='noopener noreferrer'  alt="github logo"
+            target='_blank' class="fa github">
+            &#xf092;
+            </a>
+
+            <a href="https://www.linkedin.com/in/jason-pallone-14070a187/" rel='noopener noreferrer' alt='linkedin logo' 
+            target='_blank' class="fa fa-linkedin-square" >
+            </a>
+
+            <a href="https://twitter.com/JasonPallone?s=09" rel='noopener noreferrer' alt="twitter icon"
+            target='_blank' class="fa fa-twitter-square">
+            </a>
+            
+    </footer>
+
+export default Footer
