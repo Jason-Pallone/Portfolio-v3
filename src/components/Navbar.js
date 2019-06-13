b008ef9aa86cb446521b13ce121b140175abb14f
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-13 15:08:10 -0400

    fixed background loading in chunks on home screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Navbar =()=>
     <div className='navbar'>
-        <a href="#home" className='home-button'>Home</a>
+        <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
-        <a href="#contact" className='contact'>Contact</a>
+        <a href="#contact" >Contact</a>
     </div>
 
 export default Navbar
