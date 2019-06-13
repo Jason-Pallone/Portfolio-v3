c1650974ecbcd5629b6ca6487359767017892c98
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-13 14:41:39 -0400

    fixed issue with github link on rsvp for mobile

--- src/components/RSVPLinks.js
+++ src/components/RSVPLinks.js
@@ -3,13 +3,16 @@ import React from 'react';
     
 const RSVPLinks =()=>
     <div>
+    <div className='website-div-rsvp'>
         <a href="https://jasonpallone-rsvp.com/" rel='noopener noreferrer' target="_blank" 
         alt='RSVP site link' className='rsvp-website-link'>
             <strong>
                 View the live site!
             </strong>
         </a>
+    </div>
 
+    <div className='github-div-rsvp'>
         <a href="https://github.com/Jason-Pallone/RSVP" rel='noopener noreferrer' target="_blank"
         alt='link to RSVP repo'  className='rsvp-github-link'> 
             <strong>
@@ -17,5 +20,8 @@ const RSVPLinks =()=>
             </strong>
         </a>
     </div>
+    
+    </div>
+   
 
 export default RSVPLinks
