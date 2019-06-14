01e6ad35b517991ca48d465d6086f006bcbf3320
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-14 12:10:42 -0400

    trying to get navbar to highlight the section you're on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,84 @@ import React, {Component} from 'react';
 class Navbar extends Component{
 
     state={
-        highLightHome: false,
-        highAbout: false,
-        highProjects: false,
-        highContact: false,
+        highLightHome: '',
+        highLightAbout: '',
+        highLightProjects: '',
+        highLightContact: '',
     }
 
     highLightHome = () => {
         const top = window.pageYOffset;
         if(top>100)
         this.setState({
-        highLightHome: true
+        highLightHome: 'rgb(0, 140, 255)'
         })
     }
 
+    highLightAbout = () => {
+        const top = window.pageYOffset;
+        if(top>400)
+        this.setState({
+        highLightAbout: 'rgb(0, 140, 255)',
+        highLightHome:''
+        })
+        else{
+            this.setState({
+                highLightAbout: ''
+            })
+        }
+    }
+
+    highLightProjects = () => {
+        const top = window.pageYOffset;
+        if(top>1900)
+        this.setState({
+        highLightProjects:'rgb(0, 140, 255)',
+        highLightAbout: '',
+        })
+        else{
+            this.setState({
+                highLightProjects: ''
+            })
+        }
+    }
+
+    highLightContact = () => {
+        const top = window.pageYOffset;
+        if(top>2700)
+        this.setState({
+        highLightContact:'rgb(0, 140, 255)',
+        highLightProjects:'',
+        })
+        else{
+            this.setState({
+                highLightContact: ''
+            })
+        }
+    }
+
+
     componentDidMount(){
         window.addEventListener("scroll", this.highLightHome)
+        window.addEventListener("scroll", this.highLightAbout)
+        window.addEventListener("scroll", this.highLightProjects)
+        window.addEventListener("scroll", this.highLightContact)
     }
 
     componentWillUnmount(){
         window.removeEventListener("scroll", this.highLightHome)
+        window.removeEventListener("scroll", this.highLightAbout)
+        window.removeEventListener("scroll", this.highLightProjects)
+        window.removeEventListener("scroll", this.highLightContact)
     }
 
     render(){
         return(
         <div className='navbar'>
-            <a href="#home" className={ this.state.highLightHome ? 'hightLight' : ''}>Home</a>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact" >Contact</a>
+            <a href="#home"  style={{color:`${this.state.highLightHome}`}}>Home</a>
+            <a href="#about" style={{color:`${this.state.highLightAbout}`}}>About</a>
+            <a href="#projects" style={{color:`${this.state.highLightProjects}`}}>Projects</a>
+            <a href="#contact" style={{color:`${this.state.highLightContact}`}} >Contact</a>
         </div>
         )
     }
