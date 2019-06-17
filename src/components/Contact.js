ba0c1ee02da50490d9580f7f484db128d892f1a4
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-17 15:11:44 -0400

    updated to contact header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,6 @@ const Contact = ()=>
        <h1 className='contact-header'>
             Contact me
        </h1>
-       <hr className='contact-hr'></hr>
     
         <form  className='contact-form' action='mailto:jasonpallone7593@gmail.com' method='post' encType='text/plain'>
             <input type='text' placeholder='Your name' className='contact-me-input1' name='Name'/>
