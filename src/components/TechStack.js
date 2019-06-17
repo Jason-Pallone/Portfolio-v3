59895710ac7b3a533457585d2bc979c18747264d
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-16 22:51:05 -0400

    testing a font change

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -28,7 +28,6 @@ class TechStack extends Component{
             <h1  className={ this.state.showStack ? 'stack-header showText-right' : 'hidden'}>
             My Stack
             </h1>
-            <hr  className={ this.state.showStack ? 'stack-hr showText-right' : 'hidden'}></hr>
               <ul  className={ this.state.showStack ? 'tech-stack-ul pop-in' : 'hidden'}>
                   <li><strong>HTML</strong></li>
                   <li><strong>CSS</strong></li>
