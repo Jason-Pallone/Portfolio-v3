eebd78b384380470b4062e343adc5b3d3872f962
Jason-Pallone <jasonpallone7593@gmail.com>
2019-08-17 21:43:59 -0400

    Corrected spelling mistake on nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar=()=>
         
             <a href='#home'>Home</a>
             <a href='#about'>About</a>
-            <a href='#portfolio'>portfolio</a>
+            <a href='#portfolio'>Portfolio</a>
             <a href='#contact'>Contact</a>
            
         </nav>
