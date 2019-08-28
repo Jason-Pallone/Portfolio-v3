2d5b28934a8c9238086e94ceb7abf8a953791fc3
Jason-Pallone <jasonpallone7593@gmail.com>
2019-08-28 00:29:56 -0400

    changed homepage background photo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import React from 'react';
 
 const Navbar=(props)=>
         <nav style={{background: props.showNavBackground ? 'white':'transparent'}} className= 'navbar' > 
-            <a href='#home'>Home</a>
-            <a href='#about'>About</a>
-            <a href='#portfolio'>Portfolio</a>
-            <a href='#contact'>Contact</a>
+            <a style={{color: props.showNavBackground ? 'black':'white'}} href='#home'>Home</a>
+            <a style={{color: props.showNavBackground ? 'black':'white'}} href='#about'>About</a>
+            <a style={{color: props.showNavBackground ? 'black':'white'}} href='#portfolio'>Portfolio</a>
+            <a style={{color: props.showNavBackground ? 'black':'white'}} href='#contact'>Contact</a>
         </nav>
 
 
