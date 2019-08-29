57d120389ec2cbc60d03efb6b5486f0234cbcc96
Jason-Pallone <jasonpallone7593@gmail.com>
2019-08-29 00:57:30 -0400

    added tech icons that RSVP was created with, to the RSVP section. Added box shadow to RSVP section

--- src/components/RSVP-Info.js
+++ src/components/RSVP-Info.js
@@ -7,6 +7,10 @@ const RSVPInfo =()=>
     <div>
         <img src="/RSVP-gif.gif" alt='rsvp project' className='rsvp-gif' />
         <p className= 'rsvp-description'>
+        <img src="http://img.icons8.com/color/48/000000/html-5.png" alt='HTML icon' />
+        <img src="http://img.icons8.com/color/48/000000/css3.png" alt='CSS icon' />
+        <img src="https://img.icons8.com/dotty/48/000000/react.png"  alt='React icon'></img>
+        <br></br>
             This is an RSVP app created in React. You can add a guest, edit the guest,
             remove the guest and confirm them.
         </p>
