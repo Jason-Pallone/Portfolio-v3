cad5c329ffe3d0bc86f1a4c3fa508dda56d527c8
Jason-Pallone <jasonpallone7593@gmail.com>
2019-09-11 14:40:51 -0400

    working on refactoring code, using DRY CSS and more

--- src/components/projects/contact_form/Contact_form_links.js
+++ src/components/projects/contact_form/Contact_form_links.js
@@ -4,14 +4,14 @@ const ContactFormLinks = () =>
     <div>
         <div>
         <a href="https://contact-form-jp.herokuapp.com/index.html" rel='noopener noreferrer' target="_blank"
-        alt='link to contact form demo'  className='contact-form-demo-link'> 
+        alt='link to contact form demo'  className='demo-link'> 
         <strong>View the live demo!</strong>
         </a>
         </div>
 
         <div>
         <a href="https://github.com/Jason-Pallone/Contact-sign-in-form" rel='noopener noreferrer' target="_blank"
-        alt='link to contact form github'  className='contact-form-github-link'> 
+        alt='link to contact form github'  className='github-link'> 
         <strong>View GitHub repo!</strong>
         </a>
         </div>
--- src/components/projects/rsvp/RSVPLinks.js
+++ src/components/projects/rsvp/RSVPLinks.js
@@ -6,14 +6,14 @@ const RSVPLinks =()=>
     <div>
         <div className= 'website-div-rsvp'>
             <a href="https://jasonpallone-rsvp.com/" rel='noopener noreferrer' target="_blank" 
-            alt='RSVP site link' className='rsvp-website-link'>
+            alt='RSVP site link' className='demo-link'>
             <strong>View the live demo!</strong>
             </a>
         </div>
 
         <div className='github-div-rsvp'>
             <a href="https://github.com/Jason-Pallone/RSVP" rel='noopener noreferrer' target="_blank"
-            alt='link to RSVP repo'  className='rsvp-github-link'> 
+            alt='link to RSVP repo'  className='github-link'> 
             <strong>View GitHub repo!</strong>
             </a>
         </div>
