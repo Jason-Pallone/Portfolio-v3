7ec21ce5705fae20eed79677caff7f7416f63dc8
Jason-Pallone <jasonpallone7593@gmail.com>
2019-10-21 18:21:45 -0400

    Recreated my about me section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import TechStack from './tech_stack/TechStack'
-import SubIntro from './intro/SubIntro';
-import CodingInfo from './intro/CodingInfo'
-import Zoom from 'react-reveal/Zoom'
 import Flip from 'react-reveal/Flip'
 
 const About =()=>
@@ -10,23 +7,21 @@ const About =()=>
             <h1 className='about-header'>About me</h1>
             <div  className= 'intro'>
                 <p>
-                My name is Jason Pallone, I'm a Front End Developer.
-                I love to learn and I am currently seeking a new role as a Developer.
+                Hey there! My name is Jason and I really enjoy coding. Everyday i'm working to learn something new, 
+                I enjoy creating projects and making ideas come to life. I like to take problems and create an 
+                effective solution for that problem. Once I started to learn how to code, there was no going back because 
+                it lets me do these things daily and I love it. Learning to code is one of the best decisions i've ever made.
                 </p>
 
                 <p> 
                 I have experience with JavaScript, HTML, CSS, React.js, AJAX, jQuery, Express.js, Node.js, mongoDB and Redux.
-                I also have experience with Git and GitHub.
+                I also have experience with Git, NPM and GitHub.
                 </p>
 
                 <p>
                 I am very passionate about coding and learning, I'm a life long learner who
                 loves to collaborate with teams to generate success.
                 </p>
-            <Zoom>
-              <SubIntro />
-              <CodingInfo  />
-            </Zoom>
             <Flip right>
               <TechStack /> 
             </Flip>
