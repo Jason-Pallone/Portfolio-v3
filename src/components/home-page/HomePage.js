b945fd5e6ab2686c39dd4fc94467e531411ec42a
Jason-Pallone <jasonpallone7593@gmail.com>
2019-12-05 22:17:29 -0500

    Added my updated resume and changed HomePage component to a function component

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,52 +1,29 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Navbar from  '../navbar/Navbar'
 
-class HomePage extends Component{
-    state={
-        showFrontEndDevHeader: 'none',
-        showLinks:'none',
-      }
-    
-    showFrontEndDevHeader=()=>{
-        this.setState({
-            showFrontEndDevHeader: ''
-        })
-    }
-
-    showLinks=()=>{
-        this.setState({
-            showLinks: '',
-        })
-    }
-
-    componentDidMount(){
-      setTimeout(() =>  this.showFrontEndDevHeader(), 1400);
-      setTimeout(() =>  this.showLinks(), 1900);
-    }
-
-    render(){
-      return(
-        <div>
-            <Navbar showNavBackground={this.props.showNavBackground} />
-            <div id='home' className='home-div'>
-            
-                <div className='hello-name-header-container'>
-                    <h2 className='hello-header'>Hello, I'm</h2>
-                    <h2 className='name-header'>Jason Pallone</h2>
-                </div>
-
-
-                <h2 className='frontEnd-header' > I'm a Front End Developer. </h2>
-                <a className='resume' href='../resume/Jason-Pallone.pdf' download>
-                  View my resume here!
-                </a>
-                <a className='github-profile' href='https://github.com/Jason-Pallone'
-                target="_blank" rel='noopener noreferrer' >
-                  View my GitHub here!
-                </a>
+const HomePage = (props) => {
+  return (
+    <div>
+        <Navbar showNavBackground={props.showNavBackground} />
+        <div id='home' className='home-div'>
+        
+            <div className='hello-name-header-container'>
+                <h2 className='hello-header'>Hello, I'm</h2>
+                <h2 className='name-header'>Jason Pallone</h2>
             </div>
+
+
+            <h2 className='frontEnd-header' > I'm a Front End Developer. </h2>
+            <a className='resume' href='../resume/Jason-Pallone.pdf' download>
+                View my resume here!
+            </a>
+            <a className='github-profile' href='https://github.com/Jason-Pallone'
+            target="_blank" rel='noopener noreferrer' >
+                View my GitHub here!
+            </a>
         </div>
-    )}
+    </div>
+  )
 }
 
-    export default HomePage
+export default HomePage
